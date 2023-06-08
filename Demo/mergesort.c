b00718c4ede7b4d77cd8ebb9ecfcd58d5cd44cfb
Fatema Zohra Prottyasha <58682991+FZProt@users.noreply.github.com>
2023-06-08 11:09:42 +0600

    merge_sort_function
    Adding print function and update merge_sort function

--- Demo/mergesort.c
+++ Demo/mergesort.c
@@ -1,25 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define max_size 8
 
 
-int a[] = {13,5,21,7,3,12,1,6};
-
-void merge_sort()
-{
-
+void merge(int a[], int l, int mid, int r){
 
 }
 
-int main()
-
+void merge_sort(int a[], int l, int r)
 {
-int =i;
+    int mid = r+l/2;
 
+    merge_sort(a,l,mid);
+    merge_sort(a,mid+1,r);
+
+    merge(a,l,mid,r);
+}
+
+void print(int a[], int n){
+    for(int i=0; i<n; i++){
+        printf("%d ", a[i]);
+    }printf("\n");
+}
+
+int main(){
+    int a[] = {13,5,21,7,3,12,1,6};
+    int max_size = sizeof(a)/sizeof(a[0]);
     printf("Before sort: ");
+    print(a,max_size);
 
-    for(i=0; i<max_size; i++)
-        printf("%d",a[i])
+    merge_sort(a,0,max_size-1);
 
+    printf("After sort: ");
+    print(a,max_size);
 
+    return 0;
 }
